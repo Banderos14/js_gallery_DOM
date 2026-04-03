6d467bcf768152e8f64a9f78eeb8cc4f4c2f2a0c
Banderos14 <anton.shyshenko@gmail.com>
2026-04-03 13:20:16 +0200

    feat: task_solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const mainImage = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName !== 'IMG') {
+    return;
+  }
+
+  const smallImage = e.target;
+  const link = smallImage.parentElement;
+  const largeImageUrl = link.href;
+
+  mainImage.src = largeImageUrl;
+});
